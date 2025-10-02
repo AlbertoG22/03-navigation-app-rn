358bdd804a6b99b71acdc286fa90db7c3206bb5f
AlbertoG22 <albertogarciamartinez99@gmail.com>
2025-10-02 12:22:25 -0600

    installing icons

--- src/presentation/screens/tabs/Tab1Screen.tsx
+++ src/presentation/screens/tabs/Tab1Screen.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import { HamburgerMenu } from '../../components/shared/HamburgerMenu';
+import Icon from '@react-native-vector-icons/ionicons';
 
 export const Tab1Screen = () => {
   return (
     <View>
       <HamburgerMenu />
+      <Icon name="rocket-outline" size={30} color="#900" />
       <Text>Tab1Screen</Text>
     </View>
   );
